dc86d704732d6d58ab918e2bc7cd1052a3b509cf
Olivia Murray <omurray4@hawaii.edu>
2018-11-15 13:36:08 -1000

    Add files via upload

---
+++ Inheritance/src/Football.java
@@ -0,0 +1,11 @@
+
+public class Football extends Sport {
+	Football () {
+		super("Football", 11, 6);
+	}
+	
+	void cheer() {
+		System.out.println("Touchdown!\n");
+	}
+
+}
---
+++ Inheritance/src/Volleyball.java
@@ -0,0 +1,12 @@
+
+public class Volleyball extends Sport {
+	
+	Volleyball () {
+		super("Basketball", 6, 1);		
+	}
+	
+	void cheer() {
+		System.out.println("Whooo!\n");
+	}
+
+}

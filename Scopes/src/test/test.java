118dd927276acad634d64e335e83af518cb51bca
Olivia Murray <omurray4@hawaii.edu>
2018-11-20 13:01:33 -1000

    Add files via upload

---
+++ Scopes/src/test/test.java
@@ -0,0 +1,46 @@
+package test;
+
+import acm.ACM;
+import acm.GameDev;
+import acm.GreyHats;
+
+public class test {
+
+	public static void main(String[] args) {
+
+		ACM acmBoard = new ACM("Tyler", "Ty");
+		GreyHats greyHatsBoard = new GreyHats();
+		GameDev gameDevBoard = new GameDev();
+		
+		//This should print out the ACM president and vice president
+		acmBoard.print();
+		
+		System.out.println(greyHatsBoard.president);
+		
+		//This method should not be accessible
+		greyHatsBoard.hackingMethod();
+
+		//This should print out the Grey Hats president and vice president
+		greyHatsBoard.print();
+		
+		//This should not be accessible
+		System.out.println(gameDevBoard.president);
+		//This should print out the Game Dev vice president
+		System.out.println(gameDevBoard.vicePresident);
+		
+		//This should not be able to change the president
+		gameDevBoard.setPresident("New President");
+		System.out.println(gameDevBoard.president);
+
+		//This should not be able to change the vice president
+		gameDevBoard.setVicePresident("New Vice President");
+		System.out.println(gameDevBoard.vicePresident);
+		
+		//This method should not be accessible
+		gameDevBoard.gaming();
+		//This method should not be accessible
+		gameDevBoard.gameTime("Your Name Here");
+		
+	}
+
+}
